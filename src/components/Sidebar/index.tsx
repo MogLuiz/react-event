2e7a838823595f55624d832a2c215ace26f39804
Luiz Henrique <luizhjramos@outlook.com>
2022-07-04 19:57:33 -0300

    feat(sidebar): head of the sidebar component

--- src/components/Sidebar/index.tsx
+++ src/components/Sidebar/index.tsx
@@ -1,7 +1,9 @@
 const Siderbar = () => {
   return (
     <aside className="w-[348px] bg-gray-700 p-6 border-l border-gray-600">
-      <h1>sidebar</h1>
+      <span className="font-bold text-2xl pb-6 mb-6 border-b border-gray-600 block">
+        Cronograma de aulas
+      </span>
     </aside>
   );
 };
