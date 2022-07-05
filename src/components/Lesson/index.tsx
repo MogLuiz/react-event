e8498f88cf81cdb6143131a2ba40c2beacca8c8c
Luiz Henrique <luizhjramos@outlook.com>
2022-07-04 21:26:18 -0300

    feat(Lesson): semantic structure of lesson component

--- src/components/Lesson/index.tsx
+++ src/components/Lesson/index.tsx
@@ -1,8 +1,17 @@
 const Lesson = () => {
   return (
-    <div>
-      <h1>Lesson</h1>
-    </div>
+    <a href="#">
+      <span>Terça 22 de junho 19h00</span>
+
+      <div>
+        <header>
+          <span>Conteúdo liberado</span>
+          <span>AO VIVO</span>
+        </header>
+
+        <strong>Abertura do evento Ignite Lab</strong>
+      </div>
+    </a>
   );
 };
 
