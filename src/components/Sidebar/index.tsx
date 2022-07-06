f47cbe050e8967d0c5aeb9ae90cb5279e9a15869
Luiz Henrique <luizhjramos@outlook.com>
2022-07-06 09:42:31 -0300

    feat(sidebar): get lessons query appolo client fetch

--- src/components/Sidebar/index.tsx
+++ src/components/Sidebar/index.tsx
@@ -1,6 +1,32 @@
+import { gql, useQuery } from "@apollo/client";
 import Lesson from "../Lesson";
 
+const GET_LESSONS_QUERY = gql`
+  query {
+    lessons(orderBy: availableAt_ASC, stage: PUBLISHED) {
+      title
+      lessonType
+      id
+      slug
+      availableAt
+    }
+  }
+`;
+
+type TGetLessonsQueryResponse = {
+  lessons: Array<{
+    availableAt: string;
+    id: string;
+    slug: string;
+    title: string;
+    lessonType: "live" | "class";
+  }>;
+};
+
 const Siderbar = () => {
+  const { data } = useQuery<TGetLessonsQueryResponse>(GET_LESSONS_QUERY);
+  console.log(data);
+
   return (
     <aside className="w-[348px] bg-gray-700 p-6 border-l border-gray-600">
       <span className="font-bold text-2xl pb-6 mb-6 border-b border-gray-600 block">
