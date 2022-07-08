0e868cd391c68bdf9c43d8ea737f1795aee94961
Luiz Henrique <luizhjramos@outlook.com>
2022-07-08 07:20:06 -0300

    refact(Lesson): changing native html anchor for react-router-dom's Link component

--- src/components/Lesson/index.tsx
+++ src/components/Lesson/index.tsx
@@ -1,6 +1,7 @@
 import { CheckCircle, Lock } from "phosphor-react";
 import { isPast, format } from "date-fns";
 import ptBR from "date-fns/locale/pt-BR";
+import { Link } from "react-router-dom";
 
 type TLessonProps = {
   title: string;
@@ -20,7 +21,7 @@ const Lesson = ({ availableAt, slug, title, type }: TLessonProps) => {
   );
 
   return (
-    <a href="#" className="group">
+    <Link to={`/event/lesson/${slug}`} className="group">
       <span className="text-gray-300">{availableDateFormatted}</span>
 
       <div className="rounded border border-gray-500 p-4 mt-2 group-hover:border-green-500">
@@ -44,7 +45,7 @@ const Lesson = ({ availableAt, slug, title, type }: TLessonProps) => {
 
         <strong className="text-gray-200 mt-5 block">{title}</strong>
       </div>
-    </a>
+    </Link>
   );
 };
 
