38ab4752b27c80f7eaa5307b8421d3b779066174
Luiz Henrique <luizhjramos@outlook.com>
2022-07-07 22:13:15 -0300

    feat(video): video player

--- src/components/Video/index.tsx
+++ src/components/Video/index.tsx
@@ -1,3 +1,4 @@
+import { DefaultUi, Player, Youtube } from "@vime/react";
 import {
   CaretRight,
   DiscordLogo,
@@ -9,7 +10,12 @@ const Video = () => {
   return (
     <div className="flex-1">
       <div className="bg-black flex justify-center">
-        <div className="h-full w-full max-w-[1100px] max-h-[60vh] aspect-video"></div>
+        <div className="h-full w-full max-w-[1100px] max-h-[60vh] aspect-video">
+          <Player>
+            <Youtube videoId="0PwFujd0F1I" />
+            <DefaultUi />
+          </Player>
+        </div>
       </div>
 
       <div className="p-8 max-w-[1100px] mx-auto">
